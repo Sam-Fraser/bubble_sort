c09e21708a2a72c3aa45b0af671099a1b603d4c3
Sam Eff <HeySamEff@gmail.com>
2020-06-05 13:01:54 -0700

    added pseudo code for explaining reasoning

---
+++ bubble_sort.rb
@@ -0,0 +1,9 @@
+def bubble_sort(arr)
+  #parse through the array as many times as necessary to sort the array
+  #the first parse through should put the largest number at the end
+  #subsequent parse throughs should place the next largest number at the second, third, fourth, etc. to last position.
+  #when parsing through, for each pair of numbers, compare the numbers
+  #if the first number is larger than the second number swap them
+  #move to the next set where the second number from the last set is now the first number
+  
+end
